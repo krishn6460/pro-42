3bf2f406eb0804bc3bbdaf5a7f67fcbccd01ae6a
krishn6460 <74172120+krishn6460@users.noreply.github.com>
2021-03-19 23:13:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,120 +1,50 @@
-var backImage,backgr;
-var player, player_running;
-var ground,ground_img;
-
-var FoodGroup, bananaImage;
-var obstaclesGroup, obstacle_img;
-
-var END =0;
-var PLAY =1;
-var gameState = PLAY;
-
-var gameOver; 
-var score=0;
-var attempts=3;
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var score =0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var player1score =0;
+var player2score =0;
 
 function preload(){
-  backImage=loadImage("jungle.jpg");
-  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
-  bananaImage = loadImage("banana.png");
-  obstacle_img = loadImage("stone.png"); 
-  gameOverImg = loadImage("gameOver.png");
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
 }
-
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
   
-  backgr=createSprite(0,0,800,400);
-  backgr.addImage(backImage);
-  backgr.scale=1.5;
-  backgr.x=backgr.width/2;
-  backgr.velocityX=-4;
-  
-  player = createSprite(100,340,20,50);
-  player.addAnimation("Running",player_running);
-  player.scale = 0.1;
-  
-  ground = createSprite(400,350,800,10);
-  ground.x=ground.width/2;
-  ground.visible=false;
-  
-  FoodGroup = new Group();
-  obstaclesGroup = new Group();
-  
-  score = 0;
 }
 
-function draw() { 
-  background(0);
-  drawSprites();
+function draw() {
+  background(back_img);
   
-  stroke("white");
-  textSize(20);
-  fill("white");
-  text("Score: "+ score, 550,50);
-  
-  if(gameState===PLAY){
-  
-  if(backgr.x<100){
-    backgr.x=backgr.width/2;
-  }
-  
-    if(FoodGroup.isTouching(player)){
-      FoodGroup.destroyEach();
-      player.scale += 0.05
-      score = score + 2;
-    }
-  
-    if(keyDown("space") ) {
-      player.velocityY = -12;
-    }
-    player.velocityY = player.velocityY + 0.8;
-  
-    player.collide(ground);
-    spawnFood();
-    spawnObstacles();  
- 
-    if(obstaclesGroup.isTouching(player)){ 
-        gameState = END;
-    }
-  }else if(gameState === END){
-
-    backgr.velocityX = 0;
-    player.visible = false;
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
     
-    FoodGroup.destroyEach();
-    obstaclesGroup.destroyEach();
-
-    textSize(30);
-    fill(255);
-    text("Game Over!", 300,220);
-  }
-}
-
-function spawnFood() {
-  //write code here to spawn the food
-  if (frameCount % 80 === 0) {
-    var banana = createSprite(600,250,40,10);
-    banana.y = random(120,200);    
-    banana.addImage(bananaImage);
-    banana.scale = 0.05;
-    banana.velocityX= -4; 
-    
-    banana.lifetime = 300;
-    player.depth = banana.depth + 1;
-    FoodGroup.add(banana);
-  }
-}
-
-function spawnObstacles() {
-  //write code here to spawn the obstacles
-  if(frameCount % 300 === 0) {
-    var obstacle = createSprite(800,350,10,40);
-    obstacle.velocityX=-(4 + 2*score/100); 
-    obstacle.addImage(obstacle_img);
-    
-    obstacle.scale = 0.2;
-    obstacle.lifetime = 300;
-    obstaclesGroup.add(obstacle);
-  }
+     game.end();
+   }
 }
